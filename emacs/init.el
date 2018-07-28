7b50422df6fd13857e6b6486d776d5f7ec75b039
Idir Lankri <lankri.idir@gmail.com>
2018-07-28 22:17:44 +0200

    Emacs: Give a higher priority to the GNU ELPA repository

--- emacs/init.el
+++ emacs/init.el
@@ -3,6 +3,9 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
+;; Give a higher priority to the GNU ELPA repository.
+(add-to-list 'package-archive-priorities '("gnu" . 1))
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
