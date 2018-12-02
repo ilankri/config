81b6e4599b0193a5273db1ce3949898169fb661f
Idir Lankri <lankri.idir@gmail.com>
2018-12-02 14:16:18 +0100

    Emacs config: Complete completion-ignored-extensions

--- emacs/init.el
+++ emacs/init.el
@@ -239,7 +239,7 @@
 
 (my-add-to-list 'completion-ignored-extensions
                 '("auto/" ".prv/" "_build/" ".ensime_cache/" "target/"
-                  "_client/" "_deps/" "_server/"
+                  "_client/" "_deps/" "_server/" ".sass-cache/"
                   ".d" ".native" ".byte" ".pdf"
                   ".out" ".fls" ".synctex.gz" ".rel" ".unq" ".tns"
                   ".emacs.desktop" ".emacs.desktop.lock" "_region_.tex"
