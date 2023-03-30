98830c998ce4ce4ce05aeacced0e2c7a79a1c2ed
Idir Lankri <ilankri@protonmail.com>
2023-03-30 14:19:38 +0200

    Emacs: Make the echoing of unfinished keystrokes work again

--- .config/emacs/init.ml
+++ .config/emacs/init.ml
@@ -2,4 +2,12 @@ let init () =
   Ecaml.Var.set_default_value Ecamlx.load_prefer_newer true;
   My.init ()
 
-let () = init ()
+let () =
+  let async_shutdown =
+    let open Ecaml.Funcall.Wrap in
+    "ecaml-async-shutdown" <: nullary @-> return nil
+  in
+  (* Hack to make the echoing of unfinished keystrokes work.  *)
+  async_shutdown ();
+
+  init ()
