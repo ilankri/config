980e293a06068c2dc2c15008eda27b0fb7ff5ef3
Idir Lankri <ilankri@protonmail.com>
2023-04-05 17:12:02 +0200

    Emacs: Add a missing interface file
    It completes a2c09fafc7448a1613011453899ba1f9fcd4912f.

---
+++ .config/emacs/lib/my/my.mli
@@ -0,0 +1,21 @@
+val init_package_archives : unit -> unit
+
+val hook_defun :
+  ?name:string ->
+  __POS__:Ecamlx.position ->
+  ?docstring:string ->
+  ?should_profile:bool ->
+  hook_type:'a Ecaml.Hook.Hook_type.t ->
+  returns:unit Ecaml.Value.Type.t ->
+  ('a -> unit) ->
+  'a Ecaml.Hook.Function.t
+
+module Command : sig
+  val ispell_en : unit -> Ecaml.Command.t
+  val ispell_fr : unit -> Ecaml.Command.t
+  val compile : unit -> Ecaml.Command.t
+  val indent_buffer : unit -> Ecaml.Command.t
+  val git_grep : unit -> Ecaml.Command.t
+  val transpose_windows : unit -> Ecaml.Command.t
+  val ansi_term : unit -> Ecaml.Command.t
+end
